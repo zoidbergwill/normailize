e5ffd979ed9b5572b71e240c3b833d5178980239
William Stewart <zoidbergwill@gmail.com>
2017-11-19 15:49:45 +0200

    Simplify repo

--- lib/normailize.py
+++
@@ -1,6 +0,0 @@
-from normailize import version
-from normailize import email_address
-from normailize.provider import generic
-from normailize.provider import gmail
-from normailize.provider import live
-from normailize.provider import hotmail
